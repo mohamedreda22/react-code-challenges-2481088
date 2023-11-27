1be4dad3ff967fe7800ecc1036554ab28923263c
mohamedreda22 <mohamed_aboreda_2020@yahoo.com>
2023-11-27 20:23:46 +0200

    handelSubmit

--- src/03-form-validator/FormValidator.js
+++ src/03-form-validator/FormValidator.js
@@ -4,9 +4,32 @@ export default function FormValidator () {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [passwordConfirm, setPasswordConfirm] = useState('')
+  function handelSubmit(e){
+    e.preventDefailt();
+    
+    if(validateForm()){
+      alert('Form Submitted')
+    }
+    else
+     alert('please enter a valid informaion make sure the email contain @ sign and the password is 8 or more characters long')
+  }
+  function validateForm(){
+    if(email.includes('@') && password.length >= 8 && password === passwordConfirm)
+      return true
+    else
+      return false
+  }
+  function showSubmit1(){
+    console.log(
+      'email: ', email,
+      'password: ', password   
+
+    )
+  }
+
 
   return (
-    <form>
+    <form onSubmit={handelSubmit}>
       <h2>Sign Up!</h2>
       <label htmlFor='email'>Email</label>
       <input
@@ -23,7 +46,8 @@ export default function FormValidator () {
         type='password' name='password-confirm'
         onChange={e => setPasswordConfirm(e.target.value)}
       />
-      <input type='submit' value='Submit' />
+      <input type='submit' value='Submit' onClick={showSubmit1}/>
+
     </form>
   )
 }
