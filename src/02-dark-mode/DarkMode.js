eca71331edaebab6c7c9e97f10e9d7a68b8c0a98
mohamedreda22 <mohamed_aboreda_2020@yahoo.com>
2023-11-27 19:45:55 +0200

    dark mode with two solutions

--- src/02-dark-mode/DarkMode.js
+++ src/02-dark-mode/DarkMode.js
@@ -1,8 +1,30 @@
-export default function DarkMode () {
+//first solution
+import { useState } from "react"
+//first solution without useState and using document
+export  function DarkMode1 () {    
+
+  function handelDarkMode(){
+    document.body.classList.add('dark-mode')
+  }
+  function handelWhiteMode(){
+    document.body.classList.remove('dark-mode')
+}
   return (
     <div className='page'>
-      <button className='dark-mode-button'>Dark Mode</button>
-      <button className='light-mode-button'>Light Mode</button>
+      <button className='dark-mode-button' onClick={handelDarkMode} >Dark Mode </button>
+      <button className='light-mode-button' onClick={handelWhiteMode}>Light Mode</button>
     </div>
+  )
+}
+//Secound Solution with useState
+export  function DarkMode (){
+  const[DarkMode,SetDark]=useState(false);
+
+  return(
+    <div className={`page  ${DarkMode && 'dark-mode'}`}>
+      <button className='dark-mode-button' onClick={()=>{SetDark(true)}} >Dark Mode </button>
+      <button className='light-mode-button' onClick={()=>{SetDark(false)}}>Light Mode</button>
+    </div>
+
   )
 }
--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css'
-import DarkMode from './02-dark-mode/DarkMode'
-
+import {DarkMode,DarkMode1} from './02-dark-mode/DarkMode'
 function App () {
   return (
     <div className='App'>
       <DarkMode />
+      <DarkMode1/>
     </div>
   )
 }
