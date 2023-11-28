632d6392fa38e8c1ca60e3370dc852dd3819d2b5
mohamedreda22 <mohamed_aboreda_2020@yahoo.com>
2023-11-28 14:36:52 +0200

    useRef

--- src/10-focus-input/FocusInput.js
+++ src/10-focus-input/FocusInput.js
@@ -1,8 +1,16 @@
+import { useEffect, useRef } from 'react';
+
 export default function FocusInput() {
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
   return (
     <div>
       <label htmlFor='focused-input'>Focus me on page load!</label>
-      <input name='focused-input'></input>
+      <input ref={inputRef} name='focused-input' />
     </div>
-  )
+  );
 }
